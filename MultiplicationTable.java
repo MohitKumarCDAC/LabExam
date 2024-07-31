66facfd15ea002add451b89815977e81d5e66d01
yashharde96 <yash.harde.96@gmail.com>
2024-07-31 13:04:02 +0530

    Test

---
+++ MultiplicationTable.java
@@ -0,0 +1,10 @@
+public class MultiplicationTable {
+
+    public static void main(String[] args) {
+        int number = 10; // Number to print the table of
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(number + " x " + i + " = " + (number * i));
+        }
+    }
+
+}
